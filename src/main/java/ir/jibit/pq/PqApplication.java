24a539e3352262703316fc343ff7aceb241eb54d
Alireza Pourtaghi <alirezapt@protonmail.com>
2023-09-25 16:51:17 +0330

    Initial commit.

---
+++ src/main/java/ir/jibit/pq/PqApplication.java
@@ -0,0 +1,13 @@
+package ir.jibit.pq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ir/jibit/pq/PqApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.jibit.pq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
