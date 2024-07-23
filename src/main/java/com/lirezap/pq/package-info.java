0f9cb76e32d8048aab44223e78afedfad12fd73c
Alireza Pourtaghi <alirezapt@protonmail.com>
2024-07-24 00:30:00 +0330

    Reformatting.

--- src/main/java/com/lirezap/pq/package-info.java
+++ src/main/java/com/lirezap/pq/package-info.java
@@ -21,18 +21,18 @@
  * <h2>Native postgresql access using FFM</h2>
  *
  * <p>
- * PQ is a java library that uses FFM to directly access postgresql. This library is intended to be low level,
- * minimal and with the goal of being high performance. Higher level abstractions can be built easily by using these
- * low level functions.
+ * PQ is a java library that uses FFM to directly access postgresql. This library is intended to be low level, minimal
+ * and with the goal of being high performance. Higher level abstractions can be built easily by using these low level
+ * functions.
  * </p>
  *
  * <p>
  * The main usable connectors are {@link com.lirezap.pq.PQ}, {@link com.lirezap.pq.PQX} (PQ extended),
- * {@link com.lirezap.pq.cp.PQCP} (PQ connection pool) and {@link com.lirezap.pq.cp.AsyncPQCP} (Asynchronous connection pool).
- * {@link com.lirezap.pq.PQ} is very low level FFM enabled access connector, it has methods equivalent to postgresql C
- * functions. {@link com.lirezap.pq.PQX} is based on {@link com.lirezap.pq.PQ} with some extended features.
- * {@link com.lirezap.pq.cp.PQCP} is a connection pool implementation based on {@link com.lirezap.pq.PQX} and is probably the
- * most important class of this library. {@link com.lirezap.pq.cp.AsyncPQCP} is the asynchronous version of
+ * {@link com.lirezap.pq.cp.PQCP} (PQ connection pool) and {@link com.lirezap.pq.cp.AsyncPQCP} (Asynchronous connection
+ * pool). {@link com.lirezap.pq.PQ} is very low level FFM enabled access connector, it has methods equivalent to
+ * postgresql C functions. {@link com.lirezap.pq.PQX} is based on {@link com.lirezap.pq.PQ} with some extended features.
+ * {@link com.lirezap.pq.cp.PQCP} is a connection pool implementation based on {@link com.lirezap.pq.PQX} and is
+ * probably the most important class of this library. {@link com.lirezap.pq.cp.AsyncPQCP} is the asynchronous version of
  * {@link com.lirezap.pq.cp.PQCP} that can be used for asynchronous database access.
  * </p>
  *
